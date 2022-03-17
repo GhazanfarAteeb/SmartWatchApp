9d85fad806928b0c5cf0b6ee5f49e17b2ffdc8e4
usama <dmupster>
2022-03-17 16:21:25 +0500

    UI changes in fragment_maps.xml

--- app/src/main/java/com/app/smartwatchapp/Listeners/SmartWatchListeners.java
+++ app/src/main/java/com/app/smartwatchapp/Listeners/SmartWatchListeners.java
@@ -2,5 +2,4 @@ package com.app.smartwatchapp.Listeners;
 
 public class SmartWatchListeners {
 
-
 }
--- app/src/main/java/com/app/smartwatchapp/Services/LocationServices.java
+++ app/src/main/java/com/app/smartwatchapp/Services/LocationServices.java
@@ -1,4 +1,7 @@
 package com.app.smartwatchapp.Services;
 
 public class LocationServices {
+
+
+
 }
