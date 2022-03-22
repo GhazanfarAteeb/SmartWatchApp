9f32a2f436a1e9f81eede31d37fbf39aa5440b80
Ghazanfar Ateeb <strange.person9125@gmail.com>
2022-03-22 11:21:45 +0500

    added ic_start.xml

--- app/src/main/java/com/app/smartwatchapp/AppConstants/AppConstants.java
+++ app/src/main/java/com/app/smartwatchapp/AppConstants/AppConstants.java
@@ -1,9 +1,13 @@
 package com.app.smartwatchapp.AppConstants;
 
+import android.location.Location;
+
 import com.app.smartwatchapp.Models.Watch;
 import com.app.smartwatchapp.Models.WatchReadings;
 import com.crrepa.ble.conn.CRPBleConnection;
 import com.crrepa.ble.conn.CRPBleDevice;
+
+import java.util.List;
 
 public class AppConstants {
     public static CRPBleDevice mBleDevice;
@@ -11,4 +15,5 @@ public class AppConstants {
     public static final int SCAN_PERIOD = 10 * 1000;
     public static Watch connectedWatch;
     public static WatchReadings currentWatchReadings;
+    public static List<Location> locationList;
 }
